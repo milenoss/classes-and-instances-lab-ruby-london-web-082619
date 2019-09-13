d50d1f3aec91604fd32d1b2c28535b57a67c7f97
Milen Khanal <milenkhanal@gmail.com>
2019-09-13 20:23:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,46 @@
-class Dog 
-end
 
-fido = Dog.new
-fido
 
-snoopy = Dog.new
-snoopy
 
-lassie = Dog.new
-lassie
 
-class Person
-end
 
-adele_goldberg = Person.new
-adele_goldberg
 
-alan_kay = Person.new
-alan_kay
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Dog 
+# end
+
+# fido = Dog.new
+# fido
+
+# snoopy = Dog.new
+# snoopy
+
+# lassie = Dog.new
+# lassie
+
+# class Person
+# end
+
+# adele_goldberg = Person.new
+# adele_goldberg
+
+# alan_kay = Person.new
+# alan_kay
